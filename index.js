f1e095796c5fab45112eb60b1db93ac4eada2045
Chris Myers <cmyers2015@outlook.com>
2026-06-24 00:12:54 +0100

    Update for json-seal 0.14.0 to utilise Ed25519

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import { generateKeyPair, signPayload, verifyBackup } from "json-seal";
 
-const run = async () => {
+const runRsaPss = async () => {
+  console.log("--- RSA-PSS ---");
   const { privateKey, publicKey } = await generateKeyPair();
 
   const backup = await signPayload({ id: 1000 }, privateKey, publicKey);
-
-  console.log("Original valid:", (await verifyBackup(backup)).valid,  backup);
+  console.log("Original valid:", (await verifyBackup(backup)).valid, backup);
 
   backup.payload.id = 1001;
-
-  console.log("Tampered valid:", (await verifyBackup(backup)).valid,  backup);
+  console.log("Tampered valid:", (await verifyBackup(backup)).valid, backup);
 };
 
-run();
+const runEd25519 = async () => {
+  console.log("--- Ed25519 ---");
+  const { privateKey, publicKey } = await generateKeyPair("Ed25519");
+
+  const backup = await signPayload({ id: 2000 }, privateKey, publicKey, { algorithm: "Ed25519" });
+  console.log("Original valid:", (await verifyBackup(backup)).valid, backup);
+
+  backup.payload.id = 2001;
+  console.log("Tampered valid:", (await verifyBackup(backup)).valid, backup);
+};
+
+await runRsaPss();
+await runEd25519();
